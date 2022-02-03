3d4651af3c03ca19f2ed38543b24d5dc3b86f4cf
yvette Balanga Laboro <jobibalanga@yahoo.fr>
2022-02-03 15:05:55 +0100

    import interface IHotel and fixed any[] to IHotel[],fixed  hotelFilter to _hotelFilter as private adde get,set filterHotels method in order to filtre every hotel on browser

--- src/app/hotel-list/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list/hotel-list.component.ts
@@ -1,26 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IHotel } from './hotel';
 @Component({
   selector: 'app-hotel-list',
   templateUrl: './hotel-list.component.html',
   styleUrls: ['./hotel-list.component.css']
 })
-export class HotelListComponent  {
+export class HotelListComponent  implements OnInit {
+  
 
   // constructor() { }
 
-  // ngOnInit(): void {
-  // }
+  ngOnInit(): void {
+   this.filteredHotels = this.hotels;
+   this.hotelFilter = '';
+  }
 public title = 'Liste hotel';
 
-public hotels: any[]=[
+public hotels: IHotel[]=[
   {
         "hotelId": 1,
         "hotelName": "Buea sweet life",
         "description": "Belle vue au bord de la mer",
         "price": 230.5,
         "imageUrl": "assets/img/hotel-room.jpg",
-        "rating": 3.5
+        // "rating": 3.5
   },
   {
         "hotelId": 2,
@@ -28,7 +31,7 @@ public hotels: any[]=[
         "description": "Profitez de la vue sur les montagnes",
         "price": 145.5,
         "imageUrl": "assets/img/the-interior.jpg",
-        "rating": 5
+        // "rating": 5
   },
   {
         "hotelId": 3,
@@ -36,7 +39,7 @@ public hotels: any[]=[
         "description": "Séjour complet avec service de voitures",
         "price": 120.12,
         "imageUrl": "assets/img/indoors.jpg",
-        "rating": 4
+        // "rating": 4
   },
   {
         "hotelId": 4,
@@ -44,16 +47,40 @@ public hotels: any[]=[
         "description": "Magnifique cadre pour votre séjour",
         "price": 135.12,
         "imageUrl": "assets/img/window.jpg",
-        "rating": 2.5
+        // "rating": 2.5
   }
 ];
 
 
   public showBadge: boolean = false;
 
+  private _hotelFilter = 'mot';
+
+  // public hotelFilter = 'mot';
+
+  public filteredHotels: IHotel[]=[];
+
   public toggleIsNewBadge(): void{
     this.showBadge = ! this.showBadge; 
   }
+
+  public get hotelFilter(): string{
+    return this._hotelFilter;
+  }
+
+  public set hotelFilter(filter: string){
+    this._hotelFilter=filter;
+
+    this.filteredHotels = this.hotelFilter ? this.filterHotels(this.hotelFilter) : this.hotels;
+  }
+
+private filterHotels( criteria: string): IHotel[]{
+  criteria = criteria.toLocaleLowerCase();
+  const res = this.hotels.filter(
+    (hotel: IHotel) => hotel.hotelName.toLocaleLowerCase().indexOf(criteria) !== -1
+  );
+  return res;
+}
 
 }
 
