1a5f573cf4adebe6c2a520ac3117d6efb9a6f758
yvette Balanga Laboro <jobibalanga@yahoo.fr>
2022-02-05 18:28:35 +0100

    added   @Input() up rating propriety +   @Output() up   public starRatingClicked: EventEmitter<string> = new EventEmitter<string>() +sendRating()
    into star-rating.component.ts

--- src/app/shared/components/star-rating/star-rating.component.ts
+++ src/app/shared/components/star-rating/star-rating.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-star-rating',
@@ -9,12 +10,18 @@ export class StarRatingComponent implements OnChanges {
 
 
   public starWidth!: number;// la taille que prendra l'etoile correspondante
+
+  @Input()
   public rating: number = 2; // le nombre d etoile correspondant chaque hotel: 2 è la valeur intial
- 
+  @Output()
+  public starRatingClicked: EventEmitter<string> = new EventEmitter<string>()
+
   ngOnChanges(): void {
     this.starWidth = this.rating * 125 / 5; // si la valeur de rating change on va modifier la largeur de startWidth( dc on va multiplier le rating pa la largeur totale du contenair 125 diviser par le nombre de 5 etoil possible)
   }   
-
+  public sendRating(): void{
+    this.starRatingClicked.emit(`La note est de ${this.rating}`);
+  }
 
   // constructor() { }
   // ngOnInit(): void {
