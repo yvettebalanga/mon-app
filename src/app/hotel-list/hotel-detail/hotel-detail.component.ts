ca2e0bc72d773498a6910e6df0f94c28b67f1cc7
yvette Balanga Laboro <jobibalanga@yahoo.fr>
2022-04-24 16:21:42 +0200

    work in progress

--- src/app/hotel-list/hotel-detail/hotel-detail.component.ts
+++ src/app/hotel-list/hotel-detail/hotel-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IHotel } from '../hotel-list/hotel';
+import { HotelListService } from '../hotel-list/hotel-list.service';
 
 @Component({
   selector: 'app-hotel-detail',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HotelDetailComponent implements OnInit {
 
-  constructor() { }
+  public hotel : IHotel = <IHotel>{};
+  constructor( private route: ActivatedRoute, private hotelService: HotelListService) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    this.hotelService.getHotels().subscribe((hotels: IHotel[]) => {
+      // this.hotel =  hotels.find((hotel: IHotel) => hotel.hotelId = id);
+    })
+
+
+    console.log(' id: ',id);
+  
   }
 
 }
